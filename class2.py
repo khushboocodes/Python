468ba200d4feacbe886aadf544e63e36e1e4d48b
Khushboo Khator <177356643+khushboocodes@users.noreply.github.com>
2026-09-19 18:39:12 +0530

    Day 1: Start Python learning journey with variables and strings

---
+++ class2.py
@@ -0,0 +1,52 @@
+#Class 2- Variables and Data Types
+
+print("Hello, World!")
+#i am khushboo khator
+#and i am learning python programming language
+
+"""hello this is a multi line comment
+and i am learning python programming language"""
+
+a = 12
+b = 56
+
+chacha = 45
+papa = 46
+
+# Naming Conventions
+#camel case
+
+khushbooKhator = 12
+
+#pascal case
+
+KhushbooKhator = 34
+
+#snake case
+
+khushboo_khator = 67
+
+#1 error
+
+"""1hello = 25"""
+
+#2 error 
+
+"""hello world = 45"""
+
+#3 error
+
+"""&*^%$@"""
+
+#we store data types in variables
+
+a = 45
+print(type(a))
+
+#string
+
+a = 'he said "hello"'
+
+#boolean
+
+a = True #start with capital letter
---
+++ class3.py
@@ -0,0 +1,59 @@
+#Class 3- Strings
+
+a = "NATURE"
+
+print(a[5])
+print(a[-1],a[5])
+
+a = "NATURE Beautiful"
+
+print(a[6])
+
+a = "Sheryians Coding"
+print(a[0:9:1])
+
+#there are default valuses in slicing
+
+print(a[::])
+
+
+# Task
+
+a = "Hello I am Data Scientist"
+#Extract Hello
+#Extract Data
+#Extract Scientist
+
+print(a[0:5])
+print(a[11:15])
+print(a[16:25])
+
+
+#formated strings
+
+age = 25
+des = "Data Scientist"
+
+print(f"hello my age is {age} and my designations is {des}")
+
+
+#escape sequences
+
+#end line with \n
+print("hello my name is khushboo\nand my age is 21")
+
+#tab space with \t
+print("hello my name is khushboo\tand my age is 21")
+
+#backspace with \b
+print("hello my name is khushboo\b and my age is 21")
+
+
+a = 0
+b = ""
+print(bool(a))
+print(bool(b))
+
+a = 23
+a = str(a)
+print(type(a))
